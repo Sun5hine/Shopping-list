780674f5d02fa58364fe8059e10a386baf793eb4
Sun5hine <af5kh@yahoo.com>
2019-02-07 08:18:45 +0000

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -2,10 +2,10 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 
-const ItemSchema = new Schema ({
-  name: {
-    type: String,
-    required: true
+// const ItemSchema = new Schema ({
+//   name: {
+//     type: String,
+//     required: true
   },
   date: {
     type: Date,
